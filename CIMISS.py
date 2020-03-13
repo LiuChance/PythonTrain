ddc9d2ce8e5d38c8f0869cff4bf37e2fff9120ba
LauChance <408634553@qq.com>
2020-03-13 15:16:14 +0800

    20200313

--- CIMISS.py
+++ CIMISS.py
@@ -1,24 +1,21 @@
 import requests
-import os
 import csv
+import os
 
 os.chdir('F:/')
-r = requests.get(
-    'http://10.172.89.55/cimiss-web/api?userId=BEXA_XIAN_liuchang&pwd=liu7758521&interfaceId='
-    + 'getSurfEleInRegionByTimeRange&dataCode' + '='
-    + 'SURF_CHN_MAIN_MIN&timeRange' + '='
-    + '[20191209140000,20191209140500]'
-    + '&adminCodes=' + '610100,610500' + '&elements='
-    + 'Station_Name,Datetime,Station_Id_C,TEM'
-    + '&minSeparate=' + '5' + '&dataFormat=' + 'csv')
 
+url = ('http://10.172.89.55/cimiss-web/api?userId=BEXA_XIAN_liuchang&pwd=liu7758521&interfaceId='
+       + 'getSurfEleInRegionByTimeRange&dataCode' + '='
+       + 'SURF_CHN_MAIN_MIN&timeRange' + '='
+       + '[20191209140000,20191209140500]'
+       + '&adminCodes=' + '610100,610500' + '&elements='
+       + 'Station_Name,Datetime,Station_Id_C,TEM'
+       + '&minSeparate=' + '5' + '&dataFormat=' + 'csv')
 
+data = requests.get(url)
+#print(data.headers)
+with open("pyci2.csv", 'w',encoding='gbk') as f:
+    f.write(data.text)
 
-print(r.headers)
-
-
-with open('pycimiss.csv','w') as f:
-    f_csv = csv.writer(r)
-    f_csv.writerow(headers)
-    f_csv.writerows(rows)
+#print(data.text)
 
